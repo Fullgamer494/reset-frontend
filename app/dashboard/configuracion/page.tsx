54350d02456b5ced12aa4226bb2a084666aa01dc
Fullgamer494 <fullgamer494@hotmail.com>
2026-03-10 22:38:57 -0600

    fix: problemas de buildeo

--- app/dashboard/configuracion/page.tsx
+++ app/dashboard/configuracion/page.tsx
@@ -25,7 +25,6 @@ export default function ConfiguracionPage() {
     handleTerminateSponsorship,
     setUsername,
     setAddictionType,
-    setSponsorCode,
     handleUpdateProfile,
     handleRemovePeer,
     handleAddPeer,
